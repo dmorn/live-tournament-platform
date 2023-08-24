563f6a33806b09b83ac0afbb508e901836dff22c
Philip Giuliani <philip.giuliani@keepinmind.info>
2023-08-24 22:13:18 +0200

    Use 4 columns for the grid

--- lib/ltp_web/components/core_components.ex
+++ lib/ltp_web/components/core_components.ex
@@ -512,7 +512,7 @@ defmodule LTPWeb.CoreComponents do
 
   def grid(assigns) do
     ~H"""
-    <ul role="list" class="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
+    <ul role="list" class="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-2 xl:gap-x-8">
       <%= render_slot(@inner_block) %>
     </ul>
     """
