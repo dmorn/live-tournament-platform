6e7e715502c0ef060b6505cc9122cdb47d918992
Daniel Morandini <danielmorandini@me.com>
2023-08-24 20:55:38 +0200

    Add router event

--- lib/ltp/tournament/router.ex
+++ lib/ltp/tournament/router.ex
@@ -4,5 +4,6 @@ defmodule LTP.Tournament.Router do
   dispatch(LTP.Tournament.CreateTournament, to: LTP.Tournament, identity: :id)
   dispatch(LTP.Tournament.CreatePlayer, to: LTP.Tournament, identity: :tournament_id)
   dispatch(LTP.Tournament.CreateGame, to: LTP.Tournament, identity: :tournament_id)
+  dispatch(LTP.Tournament.CloseGame, to: LTP.Tournament, identity: :tournament_id)
   dispatch(LTP.Tournament.AddScore, to: LTP.Tournament, identity: :tournament_id)
 end
